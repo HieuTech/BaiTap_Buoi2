14142a3a5636ec0a3366a9bae8abfd36bc61f2f0
Your Name <you@example.com>
2024-06-06 17:41:27 +0700

    add

---
+++ src/main/java/org/microservice/baitapbuoi2/BaiTapBuoi2Application.java
@@ -0,0 +1,13 @@
+package org.microservice.baitapbuoi2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaiTapBuoi2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaiTapBuoi2Application.class, args);
+    }
+
+}
